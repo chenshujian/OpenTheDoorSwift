95cbd777351aa38efc1f5c61769117d53dcf8443
1350496710@qq.com <aA123@qq.com>
2016-06-05 23:51:10 +0800

    1.0
    初始化工程

---
+++ OpenTheDoorSwift/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  OpenTheDoorSwift
+//
+//  Created by 树坚 on 16/6/5.
+//  Copyright © 2016年 ShuJian. All rights reserved.
+//
+
+import UIKit
+import SnapKit //aotulayout库
+import SwiftyJSON //json解析库
+import Alamofire //网络请求库
+import LTMorphingLabel //label库
+import SwiftyUserDefaults //UserDefaults库
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet var _imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
