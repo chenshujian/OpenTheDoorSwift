58ade0cda3e49659418eaeea664cb83bf9ea7d0e
shujian <shujian_app@163.com>
2018-05-21 10:10:37 +0800

    新建ROOT文件

---
+++ OpenTheDoorSwift/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  OpenTheDoorSwift
+//
+//  Created by colourlifechen on 2018/5/21.
+//  Copyright © 2018年 ShuJian. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //这是一个新建的文件
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
