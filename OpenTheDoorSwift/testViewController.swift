07658f7941bbe60e27c98a778915cc51e7c07eac
shujian <shujian_app@163.com>
2018-05-23 15:34:31 +0800

    提交2

--- OpenTheDoorSwift/testViewController.swift
+++ OpenTheDoorSwift/testViewController.swift
@@ -19,6 +19,7 @@ class testViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //2.0.0 tijiao 2
     }
     
 
